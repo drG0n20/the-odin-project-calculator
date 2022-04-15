77b78626c9cc917f80ac00245613a486714802c9
drG0n20 <marcinster@gmail.com>
2022-04-15 19:14:13 +0200

    Create funtions for add, substract, multiply and divide. Create operate function to choose an opertor

---
+++ script.js
@@ -0,0 +1,33 @@
+//1.add function
+function add(a, b) {
+  a + b;
+}
+
+//2.substract
+function substract(a, b) {
+  a - b;
+}
+
+//3.multiply
+function multiply(a, b) {
+  a * b;
+}
+
+//4.divide
+function divide(a, b) {
+  a / b;
+}
+
+function operate(operator, a, b) {
+  if (operator == "+") {
+    add(a, b);
+  } else if (operator == "-") {
+    substract(a, b);
+  } else if (operator == "*") {
+    multiply(a, b);
+  } else if (operator=="/"){
+    divide(a, b);
+  } else {
+    alert("Something went wrong! Bad operator")
+  }
+}
